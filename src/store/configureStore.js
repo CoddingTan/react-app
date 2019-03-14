d36f678ed6b0de55c7cffa8140fb61a790e19f7f
chentao <chentao@funtsui.com>
2019-03-14 10:12:48 +0800

    configureStore文件注释

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,11 +2,11 @@
  * @Author: chentao 
  * @Date: 2019-03-13 10:25:55 
  * @Last Modified by: chentao
- * @Last Modified time: 2019-03-13 18:37:12
+ * @Last Modified time: 2019-03-14 10:10:25
  */
 import { createStore,applyMiddleware } from 'redux'
 
-https://www.redux.org.cn/docs/advanced/AsyncFlow.html
+//https://www.redux.org.cn/docs/advanced/AsyncFlow.html
 //本身redux只支持同步数据流 可以使用 applyMiddleware() 来增强 createStore()  然后在Action那里如果做网络请求 就而已直接通过dispatch方法触发action给reduce接收
 import thunkMiddleware from 'redux-thunk';//
 //导入所有的reducer 注入store中
